818451d30851e90d474d8f07ffc61752f6fb0171
walisonfbalves <walisonfbalves.ti@gmail.com>
2024-08-12 04:09:07 -0300

    vetores - arrays

--- aula 3 - vetores/script.js
+++ aula 3 - vetores/script.js
@@ -1,4 +1,4 @@
-// como declarar um array
+// COMO DECLARAR UM ARRAY
 /* var array = ['string', 1, true];
 console.log(array) */
 
@@ -10,19 +10,35 @@ var array = ['string', 1, true, ['array1'], ['array2'],['array3'],['array4']]
 /* var array = ['string', 1, true, ['array1'], ['array2'],['array3'],['array4']]
 console.log(array[1]) */
 
-// manipulando arrays
+// MANIPULANDO ARRAYS
 
-//passa por todos arrays e retona na função o valor de cada
+//forEach() - intera uma array
 /* array.forEach(function(item, index){
     console.log(item,index)
 }) */
 
-//passa um novo valor ao final do array
+//push() - add um item no final do array
 /* array.push('novoitem')
 console.log(array) */
 
-// remove um item do final do array
+//pop() - remove um item no final do array
 /* array.pop()
 console.log(array) */
 
-// existem outros só pesquisar com chatGPT ou Copilot so lembre que é MANIPULAÇÃO DE ARRAY 
+//shift() - remove item no início do array;
+/* array.shift();
+console.log(array) */
+
+//unshift() - add item no início do array;
+/* array.unshift('Novo item');
+console.log(array); */
+
+//indexOf() - retorna o índice de um valor;
+/* console.log(array.indexOf(1)) */
+
+//splice() - remove ou substitui um item pelo índice;
+/* array.splice(0,3)
+console.log(array); */
+
+//slice() - retorna uma parte de um array existente;
+/* console.log(array.slice(0,2)) */
