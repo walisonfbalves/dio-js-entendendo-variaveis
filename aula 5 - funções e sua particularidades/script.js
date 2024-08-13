329d24fc4aaa09dbada904aa4ddb7c31e29c8c14
walisonfbalves <walisonfbalves.ti@gmail.com>
2024-08-13 19:24:09 -0300

    funções

---
+++ aula 5 - funções e sua particularidades/script.js
@@ -0,0 +1,19 @@
+// DECLARANDO UMA FUNÇÃO
+/* function funcao() {
+    console.log("Está mensagem está aparecendo por uma função")
+}
+
+// CHAMANDO UMA FUNÇÃO
+funcao() */
+
+// () - indica que é um objeto do tipo fuction
+// {} - indica que é um bloco de instrução
+
+
+// FUNÇÕES COM PARAMETROS
+
+/* function mensagem(parametro1, parametro2) {
+    console.log(parametro1, parametro2)
+}
+
+mensagem('Na chamada','eu declaro a funcao') */
